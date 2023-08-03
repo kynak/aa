fc2a2760ade1812b127f10e023652b736cd40496
kynak <138176111+kynak@users.noreply.github.com>
2023-08-03 15:30:52 +0700

    Add files via upload

---
+++ wp-content/themes/razia/assets/js/razia-script.js
@@ -0,0 +1,36 @@
+(function ($) {
+    "use strict";
+    $.fn.raziaAccessibleDropDown = function () {
+		var el = $(this);
+
+		$("a", el).focus(function() {
+		    $(this).parents("li").addClass("hover");
+		}).blur(function() {
+		    $(this).parents("li").removeClass("hover");
+		});
+	}
+
+    $(".menu-close").on('click', function(){
+       $("a.slicknav_btn").removeClass("slicknav_open");
+       $(".slicknav_nav").css("display", "none");
+    });
+
+    jQuery(document).ready(function($){
+    	$("#primary-menu").raziaAccessibleDropDown();
+        // Mobile Menu
+        $("#primary-menu").slicknav({
+            'allowParentLinks': true,
+            'prependTo': '.razia-responsive-menu',
+            'nestedParentLinks': false,
+            'closeOnClick': true,
+        });
+        
+        $(".menu-close").focus(function() {
+            $("a.slicknav_open").focus();
+        });
+
+        $(".masonry-post").masonry({
+            horizontalOrder: true,
+        });
+    });
+}(jQuery)); 
